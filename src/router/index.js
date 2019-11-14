3fe4adcc0802c5ad4178494b291580e7185c5da3
ROG <raintamagradeo@gmail.com>
2019-11-14 09:57:52 +0700

    UGD 11

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const DashboardLayout = () =>
-    import ( /* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue')
-
+const DashboardLayout = () => import(/* webpackChunkName: "dashboard" */'../components/dashboardLayout.vue')
 function loadView(view) {
-    return () =>
-        import ( /* webpackChunkName: "view-[request]" */ `../components/dashboardContents/${view}.vue`)
+    return () => import(/* webpackChunkName: "view-[request]" */ `../components/dashboardContents/${view}.vue`)
 }
-const routes = [{
-    path: '/',
-    component: DashboardLayout,
-    children: [{
-        name: 'UserController',
-        path: '',
-        component: loadView('userController')
-    }]
-}, ]
-Vue.use(Router)
-const router = new Router({ mode: 'history', routes: routes })
-export default router
+const routes = [
+    {
+        path: '/',
+        component: DashboardLayout,
+        children: [
+            {
+                name: 'UserController',
+                path: '/',
+                component: loadView('userController')
+            },
+            {
+                path: '/vehicle',
+                component: DashboardLayout,
+                children: [
+                    {
+                        name: 'VehicleCOntroller',
+                        path: '',
+                        component: loadView('vehicleController')
+                    }
+                ]
+            },
+        ]
+    },
+    
+ ]
+ Vue.use(Router)
+ const router = new Router({mode: 'history', routes: routes})
+ export default router
